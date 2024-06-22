84fcc421c8408d77bb98358118599ab52ada28ee
Mikołaj Kapica <mikolajkapica7@gmail.com>
2024-06-23 01:35:05 +0200

    Fix not concatenated routes

--- api/src/main/scala/Router.scala
+++ api/src/main/scala/Router.scala
@@ -3,20 +3,27 @@ import akka.http.scaladsl.model.HttpEntity
 import akka.http.scaladsl.server.Directives.complete
 import akka.http.scaladsl.server.Directives.get
 import akka.http.scaladsl.server.Directives.path
+import akka.http.scaladsl.server.Directives.pathSingleSlash
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._enhanceRouteWithConcatenation
 
 object Router {
 
   val route: Route =
-    path("hello") {
+    pathSingleSlash {
       get {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Welcome to the API</h1>"))
       }
-    }
-    path("health") {
-      get {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>OK</h1>"))
+    } ~
+      path("hello") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        }
+      } ~
+      path("health") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>OK</h1>"))
+        }
       }
-    }
 
 }
