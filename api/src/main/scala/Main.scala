13ae20d0245d2fb8f038c530f66e368bde9008b2
Mikołaj Kapica <mikolajkapica7@gmail.com>
2024-06-22 19:38:41 +0200

    Check the db connection

--- api/src/main/scala/Main.scala
+++ api/src/main/scala/Main.scala
@@ -1,37 +1,3 @@
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.Behaviors
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.*
-import akka.http.scaladsl.server.Directives.*
-
-import scala.concurrent.ExecutionContextExecutor
-import scala.io.StdIn
-
 object Main {
-
-  def main(args: Array[String]): Unit = {
-    implicit val system: ActorSystem[Any] = ActorSystem(Behaviors.empty, "my-system")
-    implicit val executionContext: ExecutionContextExecutor = system.executionContext
-
-    val host = "0.0.0.0"
-    val port = 8080
-
-    val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-        }
-      }
-
-    val bindingFuture = Http()
-      .newServerAt(host, port)
-      .bind(route)
-
-    println(s"Server online at http://$host:$port/\nPress RETURN to stop...")
-    StdIn.readLine()
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
-  }
-
+  def main(args: Array[String]): Unit = Server.run()
 }
