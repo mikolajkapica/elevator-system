ff9c866af746b85b9e2bda7534dfcfef79ec9c16
Mikołaj Kapica <mikolajkapica7@gmail.com>
2024-06-25 23:22:25 +0200

    Refactor database

--- api/src/main/scala/Database.scala
+++
@@ -1,9 +0,0 @@
-import org.mongodb.scala.MongoClient
-import org.mongodb.scala.MongoDatabase
-
-object Database {
-
-  def connect(db_name: String): MongoDatabase = MongoClient(sys.env("DATABASE_URL"))
-    .getDatabase(db_name)
-
-}
